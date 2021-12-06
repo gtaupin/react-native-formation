49c98027a306a3956b45efb01a046684a9f08385
Guillaume Taupin <gtaupin@hachette-livre.fr>
2021-12-06 11:16:00 +0100

    add login screen

--- App.tsx
+++ App.tsx
@@ -1,27 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import LoginScreen from './src/screens/LoginScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app hello!</Text>
-      <StatusBar style="auto" />
-      <View>
-        <Button mode="contained" onPress={() => console.log('Pressed')}>
-          login
-        </Button>
-      </View>
-    </View>
-  );
-}
+const App = () => {
+    return <LoginScreen />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
