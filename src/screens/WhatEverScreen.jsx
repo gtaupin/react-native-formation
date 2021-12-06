3fe6e4777301e7b34d93842a8568c1056d8b3b3b
Guillaume Taupin <gtaupin@hachette-livre.fr>
2021-12-06 16:41:04 +0100

    use real data

--- src/screens/WhatEverScreen.jsx
+++ src/screens/WhatEverScreen.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { SafeAreaView, FlatList, StyleSheet, StatusBar, Text } from 'react-native';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+import {SafeAreaView, FlatList, StyleSheet, StatusBar, Text} from 'react-native';
+import {Avatar, Button, Card, Title, Paragraph} from 'react-native-paper';
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder"/>
 import {useStarships} from '../hooks/useStarships'
 
-const Item = ({ item }) => (
+const Item = ({item}) => (
   <Card elevation={3}>
     <Card.Title title={item.name} subtitle="Card Subtitle" left={LeftContent}/>
     <Card.Content>
       <Title>{item.starship_class}</Title>
       <Paragraph>{item.consumables}</Paragraph>
     </Card.Content>
-    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <Card.Cover source={{uri: 'https://picsum.photos/700'}}/>
     <Card.Actions>
       <Button>Cancel</Button>
       <Button>Ok</Button>
@@ -20,7 +21,7 @@ const Item = ({ item }) => (
 );
 
 const WhatEverScreen = () => {
-  const { isLoading, isError, data } = useStarships();
+  const {isLoading, isError, data} = useStarships();
   console.log('isLoading', isLoading)
   console.log('isError', isError)
   console.log('data', data)
@@ -33,13 +34,21 @@ const WhatEverScreen = () => {
     );
 
     return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={data.results}
-          renderItem={renderItem}
-          keyExtractor={item => item.name}
-        />
-      </SafeAreaView>
+      <>
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={data.results}
+            renderItem={renderItem}
+            keyExtractor={item => item.name}
+            ListHeaderComponent={() => (
+              <React.Fragment>
+                <Text style={styles.listTitle}>Hello world</Text>
+              </React.Fragment>
+            )}
+            stickyHeaderIndices={[0]}
+          />
+        </SafeAreaView>
+      </>
     );
   }
 }
@@ -58,6 +67,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
+  listTitle: {
+    padding: 20,
+    color: 'white',
+    backgroundColor: 'purple',
+    fontSize: 20,
+    fontWeight: "bold"
+  }
 });
 
 export default WhatEverScreen;
