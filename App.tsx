2b4a37f91264c59e920bed8ce8fd8becb12ff290
Guillaume Taupin <gtaupin@hachette-livre.fr>
2021-12-06 11:12:17 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app hello!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          login
+        </Button>
+      </View>
     </View>
   );
 }
