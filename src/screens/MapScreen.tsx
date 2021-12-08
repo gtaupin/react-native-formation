07070e00346dba474501b261af6ec76ef20baaa8
Guillaume Taupin <gtaupin@hachette-livre.fr>
2021-12-08 11:44:35 +0100

    add animated marker on map

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,27 +1,119 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type { ProviderPropType } from "react-native-maps";
+import MapView, { AnimatedRegion, MarkerAnimated } from "react-native-maps";
 
-const MapScreen = () => {
+const screen = Dimensions.get("window");
+
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+interface MapScreenProps {
+  provider: ProviderPropType;
+}
+
+const MapScreen = (props: MapScreenProps) => {
+  const [coordinate] = useState(
+    new AnimatedRegion({
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      latitudeDelta: 0,
+      longitudeDelta: 0,
+    })
+  );
+  const [marker, setMarker] = useState(null);
+
+  function animate() {
+    const newCoordinate = {
+      latitude: LATITUDE + (Math.random() - 0.5) * (LATITUDE_DELTA / 2),
+      longitude: LONGITUDE + (Math.random() - 0.5) * (LONGITUDE_DELTA / 2),
+    };
+
+    if (Platform.OS === "android") {
+      if (marker) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        marker._component.animateMarkerToCoordinate(newCoordinate, 500);
+      }
+    } else {
+      // `useNativeDriver` defaults to false if not passed explicitly
+      coordinate.timing({ ...newCoordinate, useNativeDriver: true }).start();
+    }
+  }
+
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={styles.container}>
+      <MapView
+        provider={props.provider}
+        style={styles.map}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+      >
+        <MarkerAnimated
+          ref={(markerTmp) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            setMarker(markerTmp);
+          }}
+          coordinate={coordinate}
+        />
+      </MapView>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => animate()}
+          style={[styles.bubble, styles.button]}
+        >
+          <Text>Refresh position</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
-// eslint-disable-next-line import/no-default-export
-export default MapScreen;
-
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
   map: {
-    width: "100%",
-    height: "100%",
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 20,
+    backgroundColor: "transparent",
   },
 });
+
+// eslint-disable-next-line import/no-default-export
+export default MapScreen;
